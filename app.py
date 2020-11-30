41e6b94f11e575ee5359986d486a9ccce2f8fa90
Jans Gerald Armando Lasrodo Sitorus <69677444+jansgerald@users.noreply.github.com>
2020-11-30 23:18:00 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 from flask import Flask, request 
 import pandas as pd 
 import sqlite3
+from datetime import date
+from pandas_datareader import data
 app = Flask(__name__) 
+
 
 #home root
 @app.route('/')
 def homepage():
-    return '<h1>#Hello World this is my project to build a simple API#</h1>'
+    return (f'''
+    <br>
+    <center><h1>Hello World this is my project to build a simple API</h1></center>
+    <center><h2>ENJOY!!!</h2></center>
+    <center><img src="https://miro.medium.com/max/625/1*ZU1EQ7tYeQNQlhOhyonHFA.png" width="872" height="403"></center>
+    ''')
 
-# mendapatkan buku
-@app.route('/ambil_buku')
-def ambilbuku():
-    data = pd.read_csv('data/books_c.csv')
-    return (data.to_json()) #atau integer atau string
+# mendapatkan keseluruhan data csv <data_name> 
+@app.route('/data/get/<data_name>', methods=['GET']) 
+def get_data(data_name): 
+    data = pd.read_csv('data/' + str(data_name))
+    return (data.to_json())
+
+# mendapatkan keseluruhan database <data_name> 
+@app.route('/data2/get/<data_name>/<table_name>')
+def get_data2(data_name, table_name):
+    conn = sqlite3.connect('data/' + str(data_name))
+    data = pd.read_sql_query('SELECT * FROM ' + str(table_name), conn)
+    return (data.to_json())
 
 # toplanguage
 @app.route('/toplanguage')
@@ -29,7 +44,7 @@ def language():
 @app.route('/toprating/<rating>', methods=['GET'])
 def toprating(rating):
     data = pd.read_csv('data/books_c.csv')
-    condition = data['average_rating'] == float(rating)
+    condition = data['average_rating'] >= float(rating)
     books = data[condition]
     return books.to_json()
 
@@ -46,9 +61,24 @@ def revenue():
             parse_dates='InvoiceDate'
         )
     revenue['DayName'] = revenue['InvoiceDate'].dt.day_name()
-    rbd = revenue.groupby('DayName').agg({'Total':'sum'}).sort_values(by='Total', ascending=False).loc['Wednesday'][0]
-    return (f'''<h1>Wednesday is the day with highest revenue: {rbd}</h1>
-                ''')
+    rbd = revenue.groupby('DayName').agg({'Total':'sum'}).sort_values(by='Total', ascending=False)
+    highest_day = rbd.index[0]
+    highest_value = rbd.values[0][0]
+    return (rbd.to_json() +
+    f'''
+    <h4>So,{highest_day} is the day with highest revenue with {highest_value}.</h4>
+    ''')
+
+# BBRI vs BBCA
+@app.route('/stock')
+def stock():
+    symbol = ['BBRI.JK', 'BBCA.JK']
+    source = 'yahoo'
+    start_date = '2019-11-30'
+    end_date = date.today().strftime('%Y-%m-%d')
+    stock = data.DataReader(symbol, source, start_date, end_date)
+    vs = stock.stack().reset_index().groupby('Symbols').mean()
+    return (vs.to_json())
 
 # bestgenremusicbytotalsell
 @app.route('/bestgenres')
@@ -66,7 +96,7 @@ def genres():
         ON genres.GenreId = tracks.GenreId
         GROUP BY genres.Name
         ORDER BY Total DESC
-        LIMIT 5
+        LIMIT 10
         ''',
             conn,
             index_col='Name'
@@ -102,7 +132,7 @@ def artist():
             ON genres.GenreId = tracks.GenreId
             GROUP BY genres.Name
             ORDER BY SUM(invoices.Total) DESC
-            LIMIT 5
+            LIMIT 10
         )
         GROUP BY genres.Name, artists.Name
         ORDER BY Total DESC
@@ -112,21 +142,7 @@ def artist():
     artists = data.drop_duplicates(subset='Name').set_index('Name')
     return (artists.to_json())
 
-# mendapatkan keseluruhan data dari <data_name>
-@app.route('/data/get/<data_name>', methods=['GET']) 
-def get_data(data_name): 
-    data = pd.read_csv('data/' + str(data_name))
-    return (data.to_json())
  
-
-# mendapatkan data dengan filter nilai <value> pada kolom <column>
-@app.route('/data/get/equal/<data_name>/<column>/<value>', methods=['GET']) 
-def get_data_equal(data_name, column, value): 
-    data = pd.read_csv('data/' + str(data_name))
-    mask = data[column] == value
-    data = data[mask]
-    return (data.to_json())
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000) 
